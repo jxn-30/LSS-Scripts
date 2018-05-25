c161b98babef5a0c91860f8e0bbfe677b7810e88
KBOE2 <mail-an-kboe2@web.de>
2018-05-25 14:02:45 +0200

    Bugfix at Einatzdauer

--- einsatzdauer.min.user.js
+++ einsatzdauer.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-Einsatzdauer_compressed
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Zeigt die Einsatzdauer in der Einsatzliste an
 // @author       KBOE2
 // @match        https://www.leitstellenspiel.de/*
@@ -9,4 +9,4 @@
 // @include      https://www.leitstellenspiel.de/*
 // ==/UserScript==
 
-(function(){"use strict";let a=missionTimer;missionTimer=function(b){var c=1e3*b.date_end-new Date().getTime(),d=new Date(c-3600000),e="";0!=d.getHours()&&(10>d.getHours()&&(e+="0"),e+=d.getHours()+":"),0==d.getMinutes()?e+="00:":(10>d.getMinutes()&&(e+="0"),e+=d.getMinutes()+":"),0==d.getSeconds()?e+="00":(10>d.getSeconds()&&(e+="0"),e+=d.getSeconds()),$("#mission_overview_countdown_"+b.id).html(e),a(b)}})();
+(function(){"use strict";let a=missionTimer;missionTimer=function(b){var c=1e3*b.date_end-new Date().getTime();if(0<c){var d=new Date(c-3600000),e="";0!=d.getHours()&&(10>d.getHours()&&(e+="0"),e+=d.getHours()+":"),0==d.getMinutes()?e+="00:":(10>d.getMinutes()&&(e+="0"),e+=d.getMinutes()+":"),0==d.getSeconds()?e+="00":(10>d.getSeconds()&&(e+="0"),e+=d.getSeconds()),$("#mission_overview_countdown_"+b.id).html(e)}a(b)}})();
--- einsatzdauer.user.js
+++ einsatzdauer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-Einsatzdauer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Zeigt die Einsatzdauer in der Einsatzliste an
 // @author       KBOE2
 // @match        https://www.leitstellenspiel.de/*
@@ -16,31 +16,33 @@
 
 	missionTimer = function(t){
 		var einsatzdauer = t.date_end * 1000 - new Date().getTime();
-		var time = new Date(einsatzdauer - (1000 * 60 * 60));
-		var timeFormated = "";
-		if (time.getHours() != 0) {
-			if (time.getHours() < 10) {
-				timeFormated += "0";
+		if (einsatzdauer > 0) {
+			var time = new Date(einsatzdauer - (1000 * 60 * 60));
+			var timeFormated = "";
+			if (time.getHours() != 0) {
+				if (time.getHours() < 10) {
+					timeFormated += "0";
+				}
+				timeFormated += time.getHours() + ':';
 			}
-			timeFormated += time.getHours() + ':';
-		}
-		if (time.getMinutes() != 0) {
-			if (time.getMinutes() < 10) {
-				timeFormated += "0";
+			if (time.getMinutes() != 0) {
+				if (time.getMinutes() < 10) {
+					timeFormated += "0";
+				}
+				timeFormated += time.getMinutes() + ':';
+			} else {
+				timeFormated += "00:";
 			}
-			timeFormated += time.getMinutes() + ':';
-		} else {
-			timeFormated += "00:";
-		}
-		if (time.getSeconds() != 0) {
-			if (time.getSeconds() < 10) {
-				timeFormated += "0";
+			if (time.getSeconds() != 0) {
+				if (time.getSeconds() < 10) {
+					timeFormated += "0";
+				}
+				timeFormated += time.getSeconds();
+			} else {
+				timeFormated += "00";
 			}
-			timeFormated += time.getSeconds();
-		} else {
-			timeFormated += "00";
+			$('#mission_overview_countdown_' + t.id).html(timeFormated);
 		}
-		$('#mission_overview_countdown_' + t.id).html(timeFormated);
 		missionTimerOrig(t);
 	};
 })();
