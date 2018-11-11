42f255693b547210bd1c88774bf1e71ccfacee2e
Jan <jan.boehmerle@ledbrain.de>
2018-11-11 11:11:14 +0100

    Comaptible with LSSM MissionOut

--- einsatzdauer.min.user.js
+++ einsatzdauer.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-Einsatzdauer_compressed
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Zeigt die Einsatzdauer in der Einsatzliste an
 // @author       KBOE2
 // @match        https://www.leitstellenspiel.de/*
@@ -9,4 +9,4 @@
 // @include      https://www.leitstellenspiel.de/*
 // ==/UserScript==
 
-(function(){"use strict";let a=missionTimer;missionTimer=function(b){var c=1e3*b.date_end-new Date().getTime();if(0<c){var d=new Date(c-3600000),e="";0!=d.getHours()&&(10>d.getHours()&&(e+="0"),e+=d.getHours()+":"),0==d.getMinutes()?e+="00:":(10>d.getMinutes()&&(e+="0"),e+=d.getMinutes()+":"),0==d.getSeconds()?e+="00":(10>d.getSeconds()&&(e+="0"),e+=d.getSeconds()),$("#mission_overview_countdown_"+b.id).html(e)}a(b)}})();
+!function(){"use strict";let t=missionTimer;missionTimer=function(e){var i=1e3*e.date_end-(new Date).getTime();if(0<i){var n=new Date(i-36e5),o="";0!=n.getHours()&&(n.getHours()<10&&(o+="0"),o+=n.getHours()+":"),0!=n.getMinutes()?(n.getMinutes()<10&&(o+="0"),o+=n.getMinutes()+":"):o+="00:",0!=n.getSeconds()?(n.getSeconds()<10&&(o+="0"),o+=n.getSeconds()):o+="00",$("#mission_overview_countdown_"+e.id).html(o),$("#mission_out_"+e.id)[0]&&($("#mission_out_"+e.id).find(".btn-danger")[0]?$("#mission_overview_countdown_"+e.id).appendTo("#mission_caption_"+e.id):$("#mission_overview_countdown_"+e.id).prependTo($("#mission_bar_outer_"+e.id).parent()))}t(e)}}();
