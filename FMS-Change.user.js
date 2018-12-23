b9b44979eaea43d260915ca01d4a6065c75c2c22
Jan <jan.boehmerle@ledbrain.de>
2018-12-23 13:03:49 +0100

    Update FMS-Change.user.js

--- FMS-Change.user.js
+++ FMS-Change.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-FMS-Change
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1.0
 // @description  Bissle die LSS-FMS umschreiben
 // @author       Jan (KBOE2)
 // @match        https://www.leitstellenspiel.de/*
@@ -17,10 +17,12 @@
 (function() {
     'use strict';
 
+    const statusDescription = true; //Setzte diesen Wert auf false, um sowas wie "Auf Anfahrt", "Am Einsatzort" etc auszublenden.
+
     let radioMessageBuffer = radioMessage;
     radioMessage = function(t){
         if (t.mission_id !== 0) {
-            t.fms_text += " <br>" + $('#mission_caption_' + t.mission_id).text();
+            t.fms_text = (statusDescription ? t.fms_text + " <br>" : "") + $('#mission_caption_' + t.mission_id).text();
         }
         radioMessageBuffer(t);
     }
