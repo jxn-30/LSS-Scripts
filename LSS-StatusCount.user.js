9eec62859c00c4def990237d8d35da7fd70a3f33
Jan <jan.boehmerle@ledbrain.de>
2019-02-12 21:21:05 +0100

    Create LSS-StatusCount.user.js

---
+++ LSS-StatusCount.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         LSS-StatusCount
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Status-Zähler
+// @author       Jan (KBOE2)
+// @match        https://www.leitstellenspiel.de/*
+// @include      *://www.leitstellenspiel.de/
+// @include      *://www.leitstellenspiel.de/*
+// @include      *://www.missionchief.com/*
+// @include      *://www.missionchief.com/
+// @include      *://www.meldkamerspel.com/*
+// @include      *://www.meldkamerspel.com/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const s1 = true;
+    const s2 = true;
+    const s3 = true;
+    const s4 = true;
+    const s5 = true;
+    const s6 = true;
+    const s7 = true;
+    const s9 = true;
+
+    let radioMessageBuffer = radioMessage;
+    $("#radio_panel_heading").append("<span id='statusCount'></span>");
+    radioMessage = function(t){
+        radioMessageBuffer(t);
+        $('#statusCount').empty();
+        s1 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_1" title="Status 1: ' + $('#building_panel_body .building_list_fms_1').length + '">' + $('#building_panel_body .building_list_fms_1').length + '</span>') : null;
+        s2 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_2" title="Status 2: ' + $('#building_panel_body .building_list_fms_2').length + '">' + $('#building_panel_body .building_list_fms_2').length + '</span>') : null;
+        s3 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_3" title="Status 3: ' + $('#building_panel_body .building_list_fms_3').length + '">' + $('#building_panel_body .building_list_fms_3').length + '</span>') : null;
+        s4 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_4" title="Status 4: ' + $('#building_panel_body .building_list_fms_4').length + '">' + $('#building_panel_body .building_list_fms_4').length + '</span>') : null;
+        s5 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_5" title="Status 5: ' + $('#building_panel_body .building_list_fms_5').length + '">' + $('#building_panel_body .building_list_fms_5').length + '</span>') : null;
+        s6 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_6" title="Status 6: ' + $('#building_panel_body .building_list_fms_6').length + '">' + $('#building_panel_body .building_list_fms_6').length + '</span>') : null;
+        s7 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_7" title="Status 7: ' + $('#building_panel_body .building_list_fms_7').length + '">' + $('#building_panel_body .building_list_fms_7').length + '</span>') : null;
+        s9 ? $("#statusCount").append('<span class="building_list_fms building_list_fms_9" title="Status 9: ' + $('#building_panel_body .building_list_fms_9').length + '">' + $('#building_panel_body .building_list_fms_9').length + '</span>') : null;
+    }
+})();
