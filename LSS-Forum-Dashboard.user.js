ce1420250cb4499f06f9f9c9c79bd99c2a1820f7
Jan <jan.boehmerle@ledbrain.de>
2019-09-12 09:17:45 +0200

    Update LSS-Forum-Dashboard.user.js

--- LSS-Forum-Dashboard.user.js
+++ LSS-Forum-Dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-Forum-Dashboard
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.2.0
 // @description  Dashboard-Link im LSS-Forum
 // @author       Jan (KBOE2)
 // @include      https://forum.leitstellenspiel.de/*
@@ -12,5 +12,6 @@
     'use strict';
 
     $('ol.boxMenu')[0].innerHTML += '<li><a href="/cms/index.php?dashboard/">Dashboard</a></li>';
-    $('.menuOverlayItemList .menuOverlayItemSpacer').before('<li class="menuOverlayItem"><span class="menuOverlayItemWrapper"><a href="/cms/index.php?dashboard/" class="menuOverlayItemLink"><span class="menuOverlayItemTitle">Dashboard</span>')
+    $('.menuOverlayItemList .menuOverlayItemSpacer').before('<li class="menuOverlayItem"><span class="menuOverlayItemWrapper"><a href="/cms/index.php?dashboard/" class="menuOverlayItemLink"><span class="menuOverlayItemTitle">Dashboard</span>');
+    window.location.href === 'https://forum.leitstellenspiel.de/cms/index.php?dashboard/' && document.querySelector('.sidebar .containerContent').insertAdjacentHTML('beforeend', `<dt>Likes per Post</dt><dd>${(parseInt(document.querySelector('.sidebar .containerContent dd:nth-of-type(2)').innerText.replace(/\D+/g, '')) / parseInt(document.querySelector('.sidebar .containerContent dd:nth-of-type(1)').innerText.replace(/\D+/g, ''))).toFixed(4)}</dd>`);
 })();
