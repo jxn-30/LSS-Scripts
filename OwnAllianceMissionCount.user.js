73ec6d0eb0ab6f4a75d6be92d2d87d1c07519605
Jan <jan.boehmerle@ledbrain.de>
2019-09-23 11:06:18 +0200

    Update OwnAllianceMissionCount.user.js

--- OwnAllianceMissionCount.user.js
+++ OwnAllianceMissionCount.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         LSS-Own-Alliance-Mission-Count
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  shows number of own alliance missions
-// @author       You
+// @author       Jan (KBOE2)
 // @include      https://www.leitstellenspiel.de/
 // @grant        none
 // ==/UserScript==
@@ -13,10 +13,15 @@
 
     $('#search_input_field_missions')
         .css('width', '80%')
-        .after(`&nbsp;<span id="own_alliance_missions">Frei: ${$('#mission_list .panel-success').length}</span>`);
+        .after(`&nbsp;<span id="own_alliance_missions">Frei: ${$('#mission_list :not(.mission_deleted) .panel-success').length}</span>`);
     let missionMarkerAddOrig = missionMarkerAdd;
     missionMarkerAdd = (t) => {
         missionMarkerAddOrig(t);
-        $('#own_alliance_missions').text(`Frei: ${$('#mission_list .panel-success').length}`);
+        $('#own_alliance_missions').text(`Frei: ${$('#mission_list :not(.mission_deleted) .panel-success').length}`);
     };
+    let missionDeleteOrig = missionDelete;
+    missionDelete = (e) => {
+        missionDeleteOrig(e);
+        $('#own_alliance_missions').text(`Frei: ${$('#mission_list :not(.mission_deleted) .panel-success').length}`);
+    }
 })();
