c14e1b3807edbc04b034ebf2ad95b0abfba6cb0c
Jan <jan.boehmerle@ledbrain.de>
2019-12-29 16:09:01 +0000

    Create LSS-Forum-ExternalLink.user.js

---
+++ LSS-Forum-ExternalLink.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         LSS-Forum-ExternalLink
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Externe Links automatisch standardmäßig in neuem Tab / Fenster öffnen
+// @author       Jan (KBOE2)
+// @include      https://forum.leitstellenspiel.de/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelectorAll('a').forEach(el => !el.href.match(/^https:\/\/forum.leitstellenspiel.de/) && el.setAttribute('target', '_blank'));
+})();
