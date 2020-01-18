94852db2fe3e23f33fb8bca24fa20ed4b6f41160
Jan <jan.boehmerle@ledbrain.de>
2020-01-18 18:31:42 +0100

    Version 1.2.0

--- Mission-Alarm-Time.user.js
+++ Mission-Alarm-Time.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-Mission-Alarm-Time
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.2.0
 // @description  Shows the maximum time of all selected vehicles.
 // @author       Jan (KBOE2)
 // @include      https://www.leitstellenspiel.de/missions/*
@@ -13,8 +13,18 @@
 
     $('#mission_alarm_btn').append(`&nbsp;<spann class="mission_alarm_time"></span>`);
 
-    $("body").on("change", ".vehicle_checkbox", function() {
+    const observer = new MutationObserver((mutations, observer) => {
         $('.mission_alarm_time').text($("#mission-form .vehicle_checkbox:checked:last").parent().parent().find('td[class^="alarm_distance"]').text());
+        observer.disconnect();
+    });
+
+    $("body").on("change", ".vehicle_checkbox", function() {
+        let distanceTime = $("#mission-form .vehicle_checkbox:checked:last").parent().parent().find('td[class^="alarm_distance"]');
+        $('.mission_alarm_time').text(distanceTime.text());
+        if (distanceTime.length) {
+            distanceTime.find('.calculateTime').click();
+            observer.observe(distanceTime[0], { childList: true });
+        }
         $('.customAllianceShareText:first-of-type').click();
     });
 })();
