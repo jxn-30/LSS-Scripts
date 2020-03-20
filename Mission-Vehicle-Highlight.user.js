9c3b459ad82a2116d92f03ecb1a53ca567f71230
Jan <jan.boehmerle@ledbrain.de>
2020-03-20 10:34:39 +0100

    Create Mission-Vehicle-Highlight.user.js

---
+++ Mission-Vehicle-Highlight.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         LSS-Mission-Vehicle-Highlight
+// @version      1.0.0
+// @description  Highlights vehicles in alarm window
+// @author       Jan (KBOE2)
+// @include      https://www.leitstellenspiel.de/missions/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const colors = {
+        'FuStW': '#00f',
+    };
+
+    let types = Object.keys(colors);
+    let hatFz = false;
+
+    $('#mission_vehicle_at_mission tbody tr[id^="vehicle_row"], #mission_vehicle_driving tbody tr[id^="vehicle_row"]').each((_, row) => {
+        let vehicleType = row.querySelector('small').innerText.replace(/^\(|\)$/g, '');
+        types.forEach(type => {
+            if (vehicleType == type) $(row).css('background-color', colors[type]) && !hatFz && alert("Brudaaa, farbiges Auto im Einsatz!");
+        });
+    });
+})();
