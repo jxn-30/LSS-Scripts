4a782855c10719809de46ad9064e9b6684d55eac
Jan <jan.boehmerle@ledbrain.de>
2020-03-24 15:41:42 +0100

    Create Mission-Status-Highlight.user.js

---
+++ Mission-Status-Highlight.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         LSS-Mission-Status-Highlight
+// @version      1.0.0
+// @description  Highlights rows by status in Mission
+// @author       Jan (KBOE2)
+// @include      https://www.leitstellenspiel.de/missions/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    const colors = [
+        '#00F', // Status 1
+        '#0F0', // Status 2
+        '#FF0', // Status 3
+        '#F00', // Status 4
+        null,   // Status 5
+    ];
+
+    $('#mission_vehicle_at_mission tbody tr[id^="vehicle_row"] .building_list_fms, #mission_vehicle_driving tbody tr[id^="vehicle_row"] .building_list_fms').each((_, e) => {
+        let status = parseInt(e.innerText);
+        for (let i in colors) {
+            status == parseInt(i) + 1 && (e.parentElement.parentElement.style.background = colors[i]);
+        }
+    });
+})();
