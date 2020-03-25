992c969188242297b23869e650b431ad04291e39
Jan <jan.boehmerle@ledbrain.de>
2020-03-25 13:49:11 +0100

    Update LSS-Forum-Dashboard.user.js

--- LSS-Forum-Dashboard.user.js
+++ LSS-Forum-Dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-Forum-Dashboard
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.3.1
 // @description  Dashboard-Link im LSS-Forum
 // @author       Jan (KBOE2)
 // @include      https://forum.leitstellenspiel.de/*
@@ -10,7 +10,6 @@
 
 ! function() {
     "use strict";
-
     const e = window.location.search.match("dashboard") ? "active" : null;
     let t, a;
     if ($("ol.boxMenu")[0].innerHTML += `<li class="${e}"><a href="/cms/index.php?dashboard/">Dashboard</a></li>`, $(".menuOverlayItemList .menuOverlayItemSpacer").before(`<li class="menuOverlayItem ${e}"><span class="menuOverlayItemWrapper"><a href="/cms/index.php?dashboard/" class="menuOverlayItemLink"><span class="menuOverlayItemTitle">Dashboard</span>`), e) {
@@ -22,27 +21,31 @@
                     e.src = "https://www.amcharts.com/lib/3/serial.js", document.head.appendChild(e), window.clearInterval(r)
                 }
             }, 1e3),
-            n = window.setInterval(() => {
+            i = window.setInterval(() => {
                 if (window.AmCharts) {
                     let e = document.createElement("script");
-                    e.src = "https://www.amcharts.com/lib/3/themes/dark.js", document.head.appendChild(e), window.clearInterval(n)
+                    e.src = "https://www.amcharts.com/lib/3/themes/dark.js", document.head.appendChild(e), window.clearInterval(i)
                 }
             }, 1e3);
         t = parseInt(document.querySelector(".sidebar .containerContent dd:nth-of-type(2)").innerText.replace(/\D+/g, "")), a = parseInt(document.querySelector(".sidebar .containerContent dd:nth-of-type(1)").innerText.replace(/\D+/g, ""));
-        let d = JSON.parse(localStorage.bldiff || "{}"),
-            i = new Date,
-            l = Object.keys(d);
-        i.setTime(l.length ? l.reduce((e, t) => t > e ? t : e) : 0), new Date - i > 36e5 && (d[(new Date).getTime()] = a - t), localStorage.bldiff = JSON.stringify(d), document.querySelector(".sidebar .containerContent").insertAdjacentHTML("beforeend", `<dt>Likes per Post</dt><dd>${(t/parseInt(document.querySelector(".sidebar .containerContent dd:nth-of-type(1)").innerText.replace(/\D+/g,""))).toFixed(4)}</dd>`);
-        let s = document.querySelector(".sidebar .boxContent"),
-            o = s.getBoundingClientRect().width;
-        s.insertAdjacentHTML("beforeend", `<div id="bldiff-chart" style="width: ${o}px; height: ${o}px; background-color: #282828;" ></div>`);
+        let n = JSON.parse(localStorage.bldiff || "{}"),
+            s = new Date,
+            l = Object.keys(n);
+        s.setTime(l.length ? l.reduce((e, t) => t > e ? t : e) : 0), new Date - s > 36e5 && (n[(new Date).getTime()] = {
+            p: a,
+            l: t
+        }), localStorage.bldiff = JSON.stringify(n), document.querySelector(".sidebar .containerContent").insertAdjacentHTML("beforeend", `<dt>Likes per Post</dt><dd>${(t/a).toFixed(4)}</dd>`);
+        let d = document.querySelector(".sidebar .boxContent"),
+            o = d.getBoundingClientRect().width;
+        d.insertAdjacentHTML("beforeend", `<div id="bldiff-chart" style="width: ${o}px; height: ${o}px; background-color: #282828;" ></div><div id="pl-chart" style="width: ${o}px; height: ${o}px; background-color: #282828;" ></div>`);
         const c = (() => {
-            Object.keys(JSON.parse(localStorage.bldiff)).length;
+            let e = JSON.parse(localStorage.bldiff || "{}");
             return {
                 type: "serial",
                 categoryField: "date",
                 dataDateFormat: "YYYY-MM-DD HH:NN:SS",
                 theme: "dark",
+                creditsPosition: "bottom-right",
                 categoryAxis: {
                     minPeriod: "ss",
                     parseDates: !0
@@ -56,9 +59,9 @@
                     id: "ValueAxis-1"
                 }],
                 graphs: [{
-                    id: "t1",
-                    title: "t1",
-                    valueField: "t1"
+                    id: "bldiff",
+                    title: "bldiff",
+                    valueField: "bldiff"
                 }],
                 legend: {
                     enabled: !1,
@@ -69,19 +72,41 @@
                     size: 15,
                     text: "Abstand Posts - Likes"
                 }],
-                dataProvider: Object.keys(JSON.parse(localStorage.bldiff)).map((e, t) => {
-                    let a = new Date();
-                    return a.setTime(parseInt(e) + 36e5), {
-                        date: a,
-                        t1: JSON.parse(localStorage.bldiff)[e]
+                dataProvider: Object.keys(e).map((t, a) => {
+                    let r = new Date;
+                    return r.setTime(parseInt(t) + 36e5), {
+                        date: r,
+                        bldiff: e[t].p - e[t].l
                     }
                 })
             }
         })();
+        window.chart = c;
         let m = window.setInterval(() => {
             if (window.AmCharts.AmSerialChart) {
                 let e = document.createElement("script");
-                e.innerHTML = `AmCharts.makeChart("bldiff-chart", ${JSON.stringify(c)});`, document.body.appendChild(e), window.clearInterval(m)
+                e.async = !1, e.innerHTML = `AmCharts.makeChart("bldiff-chart", ${JSON.stringify(c)});`, window.clearInterval(m), c.graphs = [{
+                    id: "posts",
+                    title: "posts",
+                    valueField: "posts"
+                }, {
+                    id: "likes",
+                    title: "likes",
+                    valueField: "likes"
+                }], c.titles = [{
+                    id: "title",
+                    size: 15,
+                    text: "Verlauf Posts und Likes"
+                }];
+                let t = JSON.parse(localStorage.bldiff || "{}");
+                c.dataProvider = Object.keys(t).map((e, a) => {
+                    let r = new Date;
+                    return r.setTime(parseInt(e) + 36e5), {
+                        date: r,
+                        posts: t[e].p,
+                        likes: t[e].l
+                    }
+                }), c.legend.enabled = !0, e.innerHTML += `AmCharts.makeChart("pl-chart", ${JSON.stringify(c)});`, document.body.appendChild(e)
             }
         }, 1e3)
     }
