3ce4bfa728a005bbf6c07d31c51fa7cecb647b85
Jan <jan.boehmerle@ledbrain.de>
2020-05-09 21:08:13 +0200

    Update clickable-links.user.js

--- clickable-links.user.js
+++ clickable-links.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         LSS-Clickable-Links
-// @version      1.0.1
+// @version      1.0.2
 // @author       Jan (jxn_30)
-// @description  Sämtliche Links werden anklickbar
+// @description  Sämtliche Links werden anklickbar und Bilder angezeigt
 // @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/.*$/
 // ==/UserScript==
+const showImg = true;
+
 const urlRegex = /(?:(?:[A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)(?:(?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?/g;
 
 const NoneTextParentNodes = [
@@ -45,7 +47,15 @@ const clickableLinks = node =>
             const linkNode = document.createElement('a');
             linkNode.href = link.toString();
             linkNode.setAttribute('target', '_blank');
-            linkNode.textContent = link.toString();
+            if (showImg) {
+                const imgNode = document.createElement('img');
+                imgNode.src = link.toString();
+                imgNode.alt = link.toString();
+                imgNode.style.maxWidth = '10%';
+                linkNode.appendChild(imgNode);
+            } else {
+                linkNode.textContent = link.toString();
+            }
             n.parentNode.insertBefore(linkNode, n);
         });
         n.parentNode.removeChild(n);
@@ -61,7 +71,7 @@ allianceChat = e => {
     texts.forEach(text => {
         if (text) e.message += text;
         const link = links.shift();
-        if (link) e.message += `<a href="${link}" target="_blank">${link}</a>`;
+        if (link) e.message += `<a href="${link}" target="_blank">${showImg ? `<img src="${link}" alt="${link}" style="max-width: 10%;"/>` : link}</a>`;
     });
     allianceChatOriginal(e);
 };
