7af56630c4eb61d851bbd844092c7b0ebf8b5f84
Jan <jan.boehmerle@ledbrain.de>
2020-05-14 17:39:56 +0200

    Update einsatzdauer.user.js

--- einsatzdauer.user.js
+++ einsatzdauer.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         LSS-Einsatzdauer
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Zeigt die Einsatzdauer in der Einsatzliste an
 // @author       KBOE2
-// @match        https://www.leitstellenspiel.de/*
 // @grant        none
-// @include      https://www.leitstellenspiel.de/*
+// @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/.*$/
 // ==/UserScript==
 
 (function() {
