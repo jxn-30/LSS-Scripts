f53fd0a3c210c9c72092bb6b12690698d7254d08
Jan <jan.boehmerle@ledbrain.de>
2020-06-03 20:51:12 +0200

    Create Alliance-Member-Distance.user.js

---
+++ Alliance-Member-Distance.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         LSS-Alliance-Member-Distance
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Zeigt die fehlenden verdienten Credits zum nächsten Verbandsmitglied an
+// @author       Jan (jxn_30)
+// @grant        none
+// @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/verband\/mitglieder/\d+$/
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+    let prevValue;
+
+	document.querySelectorAll('tbody tr td:nth-of-type(3)').forEach(cell => {
+        const credits = parseInt(cell.textContent.trim().match(/^\d{1,3}(?:[.,]\d{3})*/)[0].replace(/\D/g, ''));
+        const distSpan = document.createElement('span');
+        distSpan.innerText = (credits - (prevValue || credits)).toLocaleString();
+        distSpan.style.color = 'red';
+        distSpan.style.marginLeft = '1em';
+        cell.appendChild(distSpan);
+        prevValue = credits;
+    });
+})();
