134efb17acc518c0f254904ab022b474357c3f2c
JRH-1997 <65117490+JRH-1997@users.noreply.github.com>
2020-06-22 20:24:41 +0200

    Update include all current versions

--- OwnAllianceMissionCount.user.js
+++ OwnAllianceMissionCount.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.1
 // @description  shows number of own alliance missions
 // @author       Jan (KBOE2)
-// @include      https://www.leitstellenspiel.de/
+// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|hatakeskuspeli\.com|missionchief-korea\.com|jocdispecerat112\.com|dispecerske-centrum\.com)\/.*$/
 // @grant        none
 // ==/UserScript==
 
