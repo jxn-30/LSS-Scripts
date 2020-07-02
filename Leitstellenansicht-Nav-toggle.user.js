db3828c36e6cf22e3e59597745a4875a24d1888a
Jan <jan.boehmerle@ledbrain.de>
2020-07-02 22:03:52 +0200

    Create Leitstellenansicht-Nav-toggle.user.js

---
+++ Leitstellenansicht-Nav-toggle.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Leitstellenansicht-Nav-toggle
+// @version      1.0.0
+// @author       Jan (jxn_30)
+// @include      https://www.leitstellenspiel.de/leitstellenansicht
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.innerHTML += `<a class="btn btn-default" style="position: fixed; top: 1rem; right: 1rem; z-index: 10000;" onclick="$('.navbar-fixed-top').toggle()">S/H</a>`
+})();
