fc207b8a125455a6b46e2fcbcb0b1ae1c3fb3960
Jan <jan.boehmerle@ledbrain.de>
2020-07-15 10:24:13 +0200

    Create Async-Prisoners.user.js

---
+++ Async-Prisoners.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         LSS-Async-Prisoners
+// @version      1.0.0
+// @description  Enables async prisoner transport in mission window
+// @author       Jan (jxn_30)
+// @include      https://www.leitstellenspiel.de/missions/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if (document.querySelector('.vehicle_prisoner_select')) {
+        const prisonersLabel = document.getElementById('h2_prisoners');
+        let currentPrisoners = parseInt(
+            prisonersLabel.textContent.trim().match(/^\d+/)[0] || '0'
+        );
+        prisonersLabel &&
+            currentPrisoners &&
+            document
+            .getElementById('mission_vehicle_at_mission')
+            .addEventListener('click', e => {
+            const target = e.target;
+            if (!target.matches('a.btn.btn-success[href^="/vehicles/"][href*="/gefangener/"], a.btn.btn-warning[href^="/vehicles/"][href*="/gefangener/"]'))
+                return;
+            e.preventDefault();
+            $.get(target.getAttribute('href'), () => {
+                const vehicleId = target.parentElement.getAttribute(
+                    'vehicle_id'
+                );
+                document
+                    .getElementById(`vehicle_row_${vehicleId}`)
+                    .remove();
+                target.parentElement.parentElement.remove();
+                currentPrisoners--;
+                prisonersLabel.textContent =
+                    prisonersLabel.textContent
+                    .trim()
+                    .replace(
+                    /^\d+/,
+                    currentPrisoners.toString()
+                ) || '';
+                if (!currentPrisoners)
+                    Array.from(
+                        document.querySelectorAll(
+                            '.vehicle_prisoner_select'
+                        )
+                    ).forEach(p => p.remove());
+            });
+        });
+    }
+})();
