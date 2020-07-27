59ad44108fa4104515116a288473e4121b3c5dd4
Jan <jan.boehmerle@ledbrain.de>
2020-07-27 15:25:59 +0200

    Update Async-Prisoners.user.js

--- Async-Prisoners.user.js
+++ Async-Prisoners.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LSS-Async-Prisoners
-// @version      1.0.1
+// @version      1.0.2
 // @description  Enables async prisoner transport in mission window
 // @author       Jan (jxn_30)
 // @include      https://www.leitstellenspiel.de/missions/*
@@ -28,11 +28,43 @@
                 const vehicleId = target.parentElement.getAttribute(
                     'vehicle_id'
                 );
+                const amount = 1;
+                let remainingCells = -1;
+                const newTextContent =
+                      target.textContent.trim().replace(
+                          /(\(.*?: )(\d+)(, .*\)$)/,
+                          (_, before, cells, after) =>
+                          `${before}${(() => {
+                              remainingCells =
+                                  parseInt(cells) - amount;
+                              return remainingCells;
+                          })()}${after}`
+                                ) || target.textContent;
+                Array.from(
+                    document.querySelectorAll(
+                        `.vehicle_prisoner_select a.btn[href$="/gefangener/${target
+                        .getAttribute('href')
+                        .match(/\d+$/)[0] || '-1'}"]`
+                                )
+                            ).forEach(cell => {
+                                cell.textContent = newTextContent;
+                                if (remainingCells <= 0) {
+                                    cell.classList.replace(
+                                        'btn-success',
+                                        'btn-danger'
+                                    );
+                                    cell.classList.replace(
+                                        'btn-warning',
+                                        'btn-danger'
+                                    );
+                                }
+                            });
+
                 document
                     .getElementById(`vehicle_row_${vehicleId}`)
                     .remove();
                 target.parentElement.parentElement.remove();
-                currentPrisoners--;
+                currentPrisoners -= amount;
                 prisonersLabel.textContent =
                     prisonersLabel.textContent
                     .trim()
