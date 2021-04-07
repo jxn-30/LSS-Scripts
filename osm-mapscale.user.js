480f2bfba7be20816c6ed48d24a3cdd1b376d7b0
Jan <jan.boehmerle@ledbrain.de>
2021-04-07 15:01:48 +0200

    Update osm-mapscale.user.js

--- osm-mapscale.user.js
+++ osm-mapscale.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         LSS-OSM-MapScale
-// @version      1.0.0
+// @version      1.0.1
 // @author       Jan (jxn_30)
 // @description  Kartenmaßstabsanzeige für OSM
-// @include      /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
+// @include      /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?(\?mapview=true)?$/
 // @run-at       document-idle
 // ==/UserScript==
 
