1abeadfc1c7e4715940d2b2cb84f0ef9cf6be750
Jan <jan.boehmerle@ledbrain.de>
2021-10-25 22:52:35 +0200

    👽️ update for mission API change

--- Mission-Less-ELW-Highlight.user.js
+++ Mission-Less-ELW-Highlight.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LSS-Mission-Less-ELW-Highlight
-// @version      1.0.3
+// @version      1.0.4
 // @description  Highlights missions in missionlist with less than required ELWs
 // @author       Jan (jxn_30)
 // @include      https://www.leitstellenspiel.de/
@@ -14,9 +14,7 @@
         .then(data => {
         const colorize = missionId => {
             const panel = document.getElementById(`mission_${missionId}`);
-            const missionType = parseInt(
-                panel.getAttribute('mission_type_id')
-            );
+            const missionType = panel.getAttribute('mission_type_id');
             const specs = data.find(m => m.id === missionType);
             const mission = document.getElementById(`mission_missing_${missionId}`);
             if (Number.isNaN(missionType) || !specs || !mission) return;
