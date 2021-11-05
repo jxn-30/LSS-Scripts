692aef43d49c1118ae97eeb964a36ee41bd25eaa
Jan <jan.boehmerle@ledbrain.de>
2021-11-05 11:21:41 +0100

    Update bsr-aao-vehiclelist.user.js

--- bsr-aao-vehiclelist.user.js
+++ bsr-aao-vehiclelist.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        LSS BSR AAO über Fahrzeugliste
-// @version     1.0.0
+// @version     1.0.1
 // @description Macht die AAO über die Fahrzeugliste in Bereitstellungsräumen
 // @author      Jan (jxn_30)
-// @include     /^https?:\/\/[www.]*(?:leitstellenspiel\.de|meldkamerspel\.com|missionchief\.com|missionchief\.co.uk|missionchief-australia\.com|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|jocdispecerat112\.com|missionchief-korea\.com)\/buildings/\d+\/?$/
+// @include     /^https?:\/\/[www.]*(?:leitstellenspiel\.de|meldkamerspel\.com|missionchief\.com|missionchief\.co.uk|missionchief-australia\.com|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|jocdispecerat112\.com|missionchief-korea\.com)\/buildings/\d+\/?(\?.*)?$/
 // @grant       none
 // ==/UserScript==
 
