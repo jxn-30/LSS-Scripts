07118a8d2e90176ff532d95acf21e13a867c4b92
Jan <jan.boehmerle@ledbrain.de>
2022-02-04 15:09:43 +0100

    Update noseasonalnavbar.user.js

--- noseasonalnavbar.user.js
+++ noseasonalnavbar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        LSS No-Seasonal-navbar
-// @version     1.0.0
-// @description Sets a dark OSM Map
+// @version     1.0.1
+// @description Removes any seasonal navbar background to the standard plain one.
 // @author      Jan (jxn_30)
 // @include     /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
 // @grant       GM_addStyle
