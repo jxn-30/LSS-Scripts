f5b6bb71670f5c5a5c8e91ecd360896d40be3bb6
Jan <jan.boehmerle@ledbrain.de>
2022-03-22 20:53:07 +0100

    Update lss-hide-application-background.user.js

--- lss-hide-application-background.user.js
+++ lss-hide-application-background.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         LSS kein Hintergrund bei Bewerbung
 // @description  Removes the green background on alliance applications
-// @version      1.0
+// @version      1.0.0
 // @author       Jan (jxn_30)
 // @match        https://www.leitstellenspiel.de/
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(`.alliance_apply_new{background: unset !important;}`)
+GM_addStyle(`.alliance_apply_new:not(.alliance_forum_new){background: unset !important;}`)
