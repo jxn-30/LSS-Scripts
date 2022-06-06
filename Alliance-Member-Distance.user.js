99265c2b9b121ca3c030e242782e5861c7f59027
Jan <jan.boehmerle@ledbrain.de>
2022-06-06 23:56:14 +0200

    Update Alliance-Member-Distance.user.js

--- Alliance-Member-Distance.user.js
+++ Alliance-Member-Distance.user.js
@@ -1,11 +1,48 @@
 // ==UserScript==
-// @name         LSS-Alliance-Member-Distance
-// @namespace    http://tampermonkey.net/
-// @version      1.0.10
-// @description  Zeigt die fehlenden verdienten Credits zum nächsten Verbandsmitglied an
-// @author       Jan (jxn_30)
-// @grant        none
-// @include      /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|(politiet\.)?nodsentralspillet|(politie\.)?meldkamerspel|operador193|(policia\.)?jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|(policja\.)?operatorratunkowy\.pl|dispetcher112\.ru|(polis\.)?larmcentralen-spelet\.se)\/verband\/mitglieder\/?\d*(\?.*)?$/
+// @name        LSS-Alliance-Member-Distance
+// @version     1.0.11
+// @description Zeigt die fehlenden verdienten Credits zum nächsten Verbandsmitglied an
+// @author      Jan (jxn_30)
+// @grant       none
+// @namespace   https://jxn.lss-manager.de/
+// @match       https://www.operacni-stredisko.cz/verband/mitglieder/*
+// @match       https://policie.operacni-stredisko.cz/verband/mitglieder/*
+// @match       https://www.alarmcentral-spil.dk/verband/mitglieder/*
+// @match       https://politi.alarmcentral-spil.dk/verband/mitglieder/*
+// @match       https://www.leitstellenspiel.de/verband/mitglieder/*
+// @match       https://polizei.leitstellenspiel.de/verband/mitglieder/*
+// @match       https://www.missionchief-australia.com/verband/mitglieder/*
+// @match       https://police.missionchief-australia.com/verband/mitglieder/*
+// @match       https://www.missionchief.co.uk/verband/mitglieder/*
+// @match       https://police.missionchief.co.uk/verband/mitglieder/*
+// @match       https://www.missionchief.com/verband/mitglieder/*
+// @match       https://police.missionchief.com/verband/mitglieder/*
+// @match       https://www.centro-de-mando.es/verband/mitglieder/*
+// @match       https://www.centro-de-mando.mx/verband/mitglieder/*
+// @match       https://www.hatakeskuspeli.com/verband/mitglieder/*
+// @match       https://poliisi.hatakeskuspeli.com/verband/mitglieder/*
+// @match       https://www.operateur112.fr/verband/mitglieder/*
+// @match       https://police.operateur112.fr/verband/mitglieder/*
+// @match       https://www.operatore112.it/verband/mitglieder/*
+// @match       https://polizia.operatore112.it/verband/mitglieder/*
+// @match       https://www.missionchief-japan.com/verband/mitglieder/*
+// @match       https://www.missionchief-korea.com/verband/mitglieder/*
+// @match       https://www.nodsentralspillet.com/verband/mitglieder/*
+// @match       https://politiet.nodsentralspillet.com/verband/mitglieder/*
+// @match       https://www.meldkamerspel.com/verband/mitglieder/*
+// @match       https://politie.meldkamerspel.com/verband/mitglieder/*
+// @match       https://www.operatorratunkowy.pl/verband/mitglieder/*
+// @match       https://policja.operatorratunkowy.pl/verband/mitglieder/*
+// @match       https://www.operador193.com/verband/mitglieder/*
+// @match       https://www.jogo-operador112.com/verband/mitglieder/*
+// @match       https://policia.jogo-operador112.com/verband/mitglieder/*
+// @match       https://www.jocdispecerat112.com/verband/mitglieder/*
+// @match       https://www.dispetcher112.ru/verband/mitglieder/*
+// @match       https://www.dispecerske-centrum.com/verband/mitglieder/*
+// @match       https://www.larmcentralen-spelet.se/verband/mitglieder/*
+// @match       https://polis.larmcentralen-spelet.se/verband/mitglieder/*
+// @match       https://www.112-merkez.com/verband/mitglieder/*
+// @match       https://www.dyspetcher101-game.com/verband/mitglieder/*
 // ==/UserScript==
 
 (async function() {
