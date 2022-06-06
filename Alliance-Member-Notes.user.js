957bba758eb6d66780030fad2631e5cdabd983ab
Jan <jan.boehmerle@ledbrain.de>
2022-06-06 23:56:16 +0200

    Update Alliance-Member-Notes.user.js

--- Alliance-Member-Notes.user.js
+++ Alliance-Member-Notes.user.js
@@ -1,11 +1,48 @@
 // ==UserScript==
-// @name         LSS-Alliance-Member-Notes
-// @namespace    http://tampermonkey.net/
-// @version      1.0.1
-// @description  Customizable Notes for each alliance member
-// @author       Jan (jxn_30)
-// @grant        none
-// @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/verband\/mitglieder\/?\d*(\?.*)?$/
+// @name        LSS-Alliance-Member-Notes
+// @version     1.0.2
+// @description Customizable Notes for each alliance member
+// @author      Jan (jxn_30)
+// @grant       none
+// @namespace   https://jxn.lss-manager.de/
+// @match       https://www.operacni-stredisko.cz/verband/mitglieder/*
+// @match       https://policie.operacni-stredisko.cz/verband/mitglieder/*
+// @match       https://www.alarmcentral-spil.dk/verband/mitglieder/*
+// @match       https://politi.alarmcentral-spil.dk/verband/mitglieder/*
+// @match       https://www.leitstellenspiel.de/verband/mitglieder/*
+// @match       https://polizei.leitstellenspiel.de/verband/mitglieder/*
+// @match       https://www.missionchief-australia.com/verband/mitglieder/*
+// @match       https://police.missionchief-australia.com/verband/mitglieder/*
+// @match       https://www.missionchief.co.uk/verband/mitglieder/*
+// @match       https://police.missionchief.co.uk/verband/mitglieder/*
+// @match       https://www.missionchief.com/verband/mitglieder/*
+// @match       https://police.missionchief.com/verband/mitglieder/*
+// @match       https://www.centro-de-mando.es/verband/mitglieder/*
+// @match       https://www.centro-de-mando.mx/verband/mitglieder/*
+// @match       https://www.hatakeskuspeli.com/verband/mitglieder/*
+// @match       https://poliisi.hatakeskuspeli.com/verband/mitglieder/*
+// @match       https://www.operateur112.fr/verband/mitglieder/*
+// @match       https://police.operateur112.fr/verband/mitglieder/*
+// @match       https://www.operatore112.it/verband/mitglieder/*
+// @match       https://polizia.operatore112.it/verband/mitglieder/*
+// @match       https://www.missionchief-japan.com/verband/mitglieder/*
+// @match       https://www.missionchief-korea.com/verband/mitglieder/*
+// @match       https://www.nodsentralspillet.com/verband/mitglieder/*
+// @match       https://politiet.nodsentralspillet.com/verband/mitglieder/*
+// @match       https://www.meldkamerspel.com/verband/mitglieder/*
+// @match       https://politie.meldkamerspel.com/verband/mitglieder/*
+// @match       https://www.operatorratunkowy.pl/verband/mitglieder/*
+// @match       https://policja.operatorratunkowy.pl/verband/mitglieder/*
+// @match       https://www.operador193.com/verband/mitglieder/*
+// @match       https://www.jogo-operador112.com/verband/mitglieder/*
+// @match       https://policia.jogo-operador112.com/verband/mitglieder/*
+// @match       https://www.jocdispecerat112.com/verband/mitglieder/*
+// @match       https://www.dispetcher112.ru/verband/mitglieder/*
+// @match       https://www.dispecerske-centrum.com/verband/mitglieder/*
+// @match       https://www.larmcentralen-spelet.se/verband/mitglieder/*
+// @match       https://polis.larmcentralen-spelet.se/verband/mitglieder/*
+// @match       https://www.112-merkez.com/verband/mitglieder/*
+// @match       https://www.dyspetcher101-game.com/verband/mitglieder/*
 // ==/UserScript==
 
 (function() {
