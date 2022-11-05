e1c5f358713202a56c1922269f1d94e4fd6cff3f
jan <jan.boehmerle@ledbrain.de>
2022-11-05 19:05:02 +0100

    feat(rewrite): begin to improve & rewrite all scripts

---
+++ util/games.js
@@ -0,0 +1,140 @@
+module.exports = {
+    games: {
+        cs_CZ: {
+            flag: '🇨🇿',
+            name: 'Operacni-Stredisko.cz',
+            shortURL: 'operacni-stredisko.cz',
+            police: 'policie',
+        },
+        da_DK: {
+            flag: '🇩🇰',
+            name: 'Alarmcentral-Spil.dk',
+            shortURL: 'alarmcentral-spil.dk',
+            police: 'politi',
+        },
+        de_DE: {
+            flag: '🇩🇪',
+            name: 'Leitstellenspiel.de',
+            shortURL: 'leitstellenspiel.de',
+            police: 'polizei',
+        },
+        en_AU: {
+            flag: '🇦🇺',
+            name: 'Missionchief-Australia.com',
+            shortURL: 'missionchief-australia.com',
+            police: 'police',
+        },
+        en_GB: {
+            flag: '🇬🇧',
+            name: 'Missionchief.co.uk',
+            shortURL: 'missionchief.co.uk',
+            police: 'police',
+        },
+        en_US: {
+            flag: '🇺🇸',
+            name: 'Missionchief.com',
+            shortURL: 'missionchief.com',
+            police: 'police',
+        },
+        es_ES: {
+            flag: '🇪🇸',
+            name: 'Centro-de-Mando.es',
+            shortURL: 'centro-de-mando.es',
+        },
+        es_MX: {
+            flag: '🇲🇽',
+            name: 'Centro-de-Mando.mx',
+            shortURL: 'centro-de-mando.mx',
+        },
+        fi_FI: {
+            flag: '🇫🇮',
+            name: 'Hätäkeskuspeli.com',
+            shortURL: 'hatakeskuspeli.com',
+            police: 'poliisi',
+        },
+        fr_FR: {
+            flag: '🇫🇷',
+            name: 'Operateur112.fr',
+            shortURL: 'operateur112.fr',
+            police: 'police',
+        },
+        it_IT: {
+            flag: '🇮🇹',
+            name: 'Operatore112.it',
+            shortURL: 'operatore112.it',
+            police: 'polizia',
+        },
+        ja_JP: {
+            flag: '🇯🇵',
+            name: 'Missionchief-Japan.com',
+            shortURL: 'missionchief-japan.com',
+        },
+        ko_KR: {
+            flag: '🇰🇷',
+            name: 'Missionchief-Korea.com',
+            shortURL: 'missionchief-korea.com',
+        },
+        nb_NO: {
+            flag: '🇳🇴',
+            name: 'Nodsentralspillet.com',
+            shortURL: 'nodsentralspillet.com',
+            police: 'politiet',
+        },
+        nl_NL: {
+            flag: '🇳🇱',
+            name: 'Meldkamerspel.com',
+            shortURL: 'meldkamerspel.com',
+            police: 'politie',
+        },
+        pl_PL: {
+            flag: '🇵🇱',
+            name: 'Operatorraunkowy.pl',
+            shortURL: 'operatorratunkowy.pl',
+            police: 'policja',
+        },
+        pt_BR: {
+            flag: '🇧🇷',
+            name: 'Operador193.com',
+            shortURL: 'operador193.com',
+        },
+        pt_PT: {
+            flag: '🇵🇹',
+            locale_fallback: 'en_US',
+            name: 'Jogo-operador112.com',
+            shortURL: 'jogo-operador112.com',
+            police: 'policia',
+        },
+        ro_RO: {
+            flag: '🇷🇴',
+            name: 'Jocdispecerat112.com',
+            shortURL: 'jocdispecerat112.com',
+        },
+        ru_RU: {
+            flag: '🇷🇺',
+            name: 'Dispetcher112.ru',
+            shortURL: 'dispetcher112.ru',
+        },
+        sk_SK: {
+            flag: '🇸🇰',
+            locale_fallback: 'en_US',
+            name: 'Dispečerskom-centrum.com',
+            shortURL: 'dispecerske-centrum.com',
+        },
+        sv_SE: {
+            flag: '🇸🇪',
+            name: 'Larmcentralen-spelet.se',
+            shortURL: 'larmcentralen-spelet.se',
+            police: 'polis',
+        },
+        tr_TR: {
+            flag: '🇹🇷',
+            name: '112-Merkez.com',
+            shortURL: '112-merkez.com',
+        },
+        uk_UA: {
+            flag: '🇺🇦',
+            name: 'Dyspetcher101-game.com',
+            shortURL: 'dyspetcher101-game.com',
+        },
+    },
+};
---
+++ util/updateRepo.js
@@ -0,0 +1,187 @@
+const fs = require('fs');
+const path = require('path');
+const jsdoc = require('jsdoc-api');
+
+const { games } = require('./games');
+
+/**
+ * @typedef Comment
+ * @property {string} name
+ * @property {string} description
+ * @property {Tag[]} tags
+ * @property {CommentMeta} meta
+ */
+
+/**
+ * @typedef Tag
+ * @property {string} originalTitle
+ * @property {string} title
+ * @property {string} text
+ * @property {string} value
+ */
+
+/**
+ * @typedef CommentMeta
+ * @property {string} filename
+ * @property {string} path
+ */
+
+const ROOT_PATH = path.resolve(__dirname, '..');
+const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+
+const updatedFiles = [];
+
+/**
+ * Get Tags and their localized versions
+ * @param {Tag[]} tags
+ * @param {string} title
+ * @param {string} [defaultContent]
+ * @returns {{tag: string, content: string}[]}
+ */
+const filterTags = (tags, title, defaultContent) => {
+    const matchingTags = tags.filter(
+        tag => tag.title === title || tag.title.startsWith(`${title}:`)
+    );
+    if (!matchingTags.length && !defaultContent) return [];
+    if (!matchingTags.length) return [{ tag: title, content: defaultContent }];
+    return matchingTags.map(({ title, value }) => ({
+        tag: title,
+        content: value,
+    }));
+};
+
+// Delete all symbolic links to userscripts in root directory
+fs.readdirSync(ROOT_PATH, { withFileTypes: true }).forEach(dirent => {
+    if (!dirent.isSymbolicLink() || !dirent.name.endsWith('.user.js')) return;
+    fs.unlinkSync(path.resolve(ROOT_PATH, dirent.name));
+});
+
+/** @type {Comment[]} **/
+const comments = jsdoc.explainSync({
+    files: fs.readdirSync(SRC_PATH).map(file => path.resolve(SRC_PATH, file)),
+});
+comments.forEach(comment => {
+    const fileName = comment.meta?.filename;
+    if (!fileName || updatedFiles.includes(fileName)) return;
+
+    const filePath = path.resolve(comment.meta.path, fileName);
+
+    const { tags } = comment;
+
+    const updateURL = `https://github.com/jxn-30/LSS-Scripts/raw/master/src/${fileName}`;
+
+    /**
+     * Get Tags and their localized versions
+     * @param {string} title
+     * @param {string} [defaultContent]
+     * @returns {{tag: string, content: string}[]}
+     */
+    const getTags = (title, defaultContent) =>
+        filterTags(tags, title, defaultContent);
+
+    // add symbolic links
+    getTags('old').forEach(({ content }) =>
+        fs.symlinkSync(
+            path.relative(ROOT_PATH, filePath),
+            path.resolve(ROOT_PATH, `${content}.user.js`)
+        )
+    );
+
+    // get paths to execute on
+    const locales = getTags('locale');
+    const pathMatches = getTags('match', '/*');
+
+    const matches = Object.keys(games)
+        .filter(
+            game =>
+                locales.length === 0 ||
+                locales.some(({ content }) => content === game)
+        )
+        .flatMap(game => {
+            const { shortURL, police } = games[game];
+            const matches = [];
+            if (shortURL) {
+                pathMatches.forEach(({ content: path }) =>
+                    matches.push({
+                        tag: 'match',
+                        content: `https://www.${shortURL}${path}`,
+                    })
+                );
+                if (police)
+                    pathMatches.forEach(({ content: path }) =>
+                        matches.push({
+                            tag: 'match',
+                            content: `https://${police}.${shortURL}${path}`,
+                        })
+                    );
+            }
+            return matches;
+        });
+
+    // list of tags to add to the userscript
+    const userscriptHeaderInformation = [
+        {
+            tag: 'name',
+            content: `[LSS] ${comment.name}`,
+        },
+        ...tags
+            .filter(tag => tag.title.startsWith('name:'))
+            .map(({ title, value }) => ({ tag: title, content: value })),
+        {
+            tag: 'namespace',
+            content: 'https://jxn.lss-manager.de',
+        },
+        ...getTags('version', `${new Date().getFullYear()}.0.0`),
+        ...getTags('author', 'Jan (jxn_30)'),
+        {
+            tag: 'description',
+            content: comment.description,
+        },
+        ...getTags('description'),
+        {
+            tag: 'homepage',
+            content: 'https://github.com/jxn-30/LSS-Scripts',
+        },
+        {
+            tag: 'homepageURL',
+            content: 'https://github.com/jxn-30/LSS-Scripts',
+        },
+        {
+            tag: 'updateURL',
+            content: updateURL,
+        },
+        {
+            tag: 'downloadURL',
+            content: updateURL,
+        },
+        ...matches,
+        ...getTags('run-at', 'document-idle'),
+        ...getTags('grant'),
+    ];
+
+    const longestTagLength = Math.max(
+        ...userscriptHeaderInformation.map(({ tag }) => tag.length)
+    );
+
+    const userscriptTags = userscriptHeaderInformation
+        .map(
+            ({ tag, content }) =>
+                `// @${tag.padEnd(longestTagLength, ' ')}  ${content}`
+        )
+        .join('\n');
+
+    // write Header to userscript
+    fs.writeFileSync(
+        filePath,
+        fs.readFileSync(filePath, 'utf8').replace(
+            /^\/\/ ==UserScript==.*?\/\/ ==\/UserScript==/gs,
+            `
+// ==UserScript==
+${userscriptTags}
+// ==/UserScript==
+`.trim()
+        )
+    );
+
+    updatedFiles.push(fileName);
+});
