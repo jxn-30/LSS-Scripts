4c691f95f535b895f4930c9d1fdddc996ffb0ac0
Jan <jan.boehmerle@ledbrain.de>
2022-11-05 16:07:49 +0100

    Update FMS-Change.user.js

--- FMS-Change.user.js
+++ FMS-Change.user.js
@@ -1,16 +1,48 @@
 // ==UserScript==
 // @name         LSS-FMS-Change
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  Bissle die LSS-FMS umschreiben
-// @author       Jan (KBOE2)
+// @author       Jan (jxn_30)
 // @match        https://www.leitstellenspiel.de/*
-// @include      *://www.leitstellenspiel.de/
-// @include      *://www.leitstellenspiel.de/*
-// @include      *://www.missionchief.com/*
-// @include      *://www.missionchief.com/
-// @include      *://www.meldkamerspel.com/*
-// @include      *://www.meldkamerspel.com/
+// @match        https://www.operacni-stredisko.cz/*
+// @match        https://policie.operacni-stredisko.cz/*
+// @match        https://www.alarmcentral-spil.dk/*
+// @match        https://politi.alarmcentral-spil.dk/*
+// @match        https://www.leitstellenspiel.de/*
+// @match        https://polizei.leitstellenspiel.de/*
+// @match        https://www.missionchief-australia.com/*
+// @match        https://police.missionchief-australia.com/*
+// @match        https://www.missionchief.co.uk/*
+// @match        https://police.missionchief.co.uk/*
+// @match        https://www.missionchief.com/*
+// @match        https://police.missionchief.com/*
+// @match        https://www.centro-de-mando.es/*
+// @match        https://www.centro-de-mando.mx/*
+// @match        https://www.hatakeskuspeli.com/*
+// @match        https://poliisi.hatakeskuspeli.com/*
+// @match        https://www.operateur112.fr/*
+// @match        https://police.operateur112.fr/*
+// @match        https://www.operatore112.it/*
+// @match        https://polizia.operatore112.it/*
+// @match        https://www.missionchief-japan.com/*
+// @match        https://www.missionchief-korea.com/*
+// @match        https://www.nodsentralspillet.com/*
+// @match        https://politiet.nodsentralspillet.com/*
+// @match        https://www.meldkamerspel.com/*
+// @match        https://politie.meldkamerspel.com/*
+// @match        https://www.operatorratunkowy.pl/*
+// @match        https://policja.operatorratunkowy.pl/*
+// @match        https://www.operador193.com/*
+// @match        https://www.jogo-operador112.com/*
+// @match        https://policia.jogo-operador112.com/*
+// @match        https://www.jocdispecerat112.com/*
+// @match        https://www.dispetcher112.ru/*
+// @match        https://www.dispecerske-centrum.com/*
+// @match        https://www.larmcentralen-spelet.se/*
+// @match        https://polis.larmcentralen-spelet.se/*
+// @match        https://www.112-merkez.com/*
+// @match        https://www.dyspetcher101-game.com/*
 // ==/UserScript==
 
 
