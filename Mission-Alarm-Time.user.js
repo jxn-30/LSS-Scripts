6add8e41f7f648425136660698b14d7f7f70edba
Jan <jan.boehmerle@ledbrain.de>
2022-11-05 16:12:26 +0100

    Update Mission-Alarm-Time.user.js

--- Mission-Alarm-Time.user.js
+++ Mission-Alarm-Time.user.js
@@ -1,10 +1,47 @@
 // ==UserScript==
 // @name         LSS-Mission-Alarm-Time
 // @namespace    http://tampermonkey.net/
-// @version      1.2.0
+// @version      1.2.1
 // @description  Shows the maximum time of all selected vehicles.
-// @author       Jan (KBOE2)
-// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|hatakeskuspeli\.com|missionchief-korea\.com|jocdispecerat112\.com|dispecerske-centrum\.com)\/missions\/.*$/
+// @author       Jan (jxn_30)
+// @match        https://www.operacni-stredisko.cz/missions/*
+// @match        https://policie.operacni-stredisko.cz/missions/*
+// @match        https://www.alarmcentral-spil.dk/missions/*
+// @match        https://politi.alarmcentral-spil.dk/missions/*
+// @match        https://www.leitstellenspiel.de/missions/*
+// @match        https://polizei.leitstellenspiel.de/missions/*
+// @match        https://www.missionchief-australia.com/missions/*
+// @match        https://police.missionchief-australia.com/missions/*
+// @match        https://www.missionchief.co.uk/missions/*
+// @match        https://police.missionchief.co.uk/missions/*
+// @match        https://www.missionchief.com/missions/*
+// @match        https://police.missionchief.com/missions/*
+// @match        https://www.centro-de-mando.es/missions/*
+// @match        https://www.centro-de-mando.mx/missions/*
+// @match        https://www.hatakeskuspeli.com/missions/*
+// @match        https://poliisi.hatakeskuspeli.com/missions/*
+// @match        https://www.operateur112.fr/missions/*
+// @match        https://police.operateur112.fr/missions/*
+// @match        https://www.operatore112.it/missions/*
+// @match        https://polizia.operatore112.it/missions/*
+// @match        https://www.missionchief-japan.com/missions/*
+// @match        https://www.missionchief-korea.com/missions/*
+// @match        https://www.nodsentralspillet.com/missions/*
+// @match        https://politiet.nodsentralspillet.com/missions/*
+// @match        https://www.meldkamerspel.com/missions/*
+// @match        https://politie.meldkamerspel.com/missions/*
+// @match        https://www.operatorratunkowy.pl/missions/*
+// @match        https://policja.operatorratunkowy.pl/missions/*
+// @match        https://www.operador193.com/missions/*
+// @match        https://www.jogo-operador112.com/missions/*
+// @match        https://policia.jogo-operador112.com/missions/*
+// @match        https://www.jocdispecerat112.com/missions/*
+// @match        https://www.dispetcher112.ru/missions/*
+// @match        https://www.dispecerske-centrum.com/missions/*
+// @match        https://www.larmcentralen-spelet.se/missions/*
+// @match        https://polis.larmcentralen-spelet.se/missions/*
+// @match        https://www.112-merkez.com/missions/*
+// @match        https://www.dyspetcher101-game.com/missions/*
 // @grant        none
 // ==/UserScript==
 
