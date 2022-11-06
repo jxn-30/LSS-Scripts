b685f119f8f1c2cebea892e2fd644c224e54f38b
jan <jan.boehmerle@ledbrain.de>
2022-11-06 01:00:26 +0100

    feat: auto-generate readme as overview

--- README.md
+++ README.md
@@ -1,3 +1,143 @@
-# LSS-Scripts
+## [LSS] AAO Config Lite
 
-This repo contains all my scripts for the game. Automated list will be added later.
+> This Script is for leitstellenspiel.de only!
+
+| Version  | Available in | Alias / Old names |                       Download                       |
+|:--------:|:------------:|:-----------------:|:----------------------------------------------------:|
+| 2022.0.0 |    `🇩🇪`    |   `aao-lite-de`   | [aaoConfigLite.de.user.js][aaoConfigLite.de.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] AAO Config Lite</summary>
+    Blendet Eingabefelder in der AAO-Konfiguration nach Bedarf ein oder aus.
+</details>
+
+[aaoConfigLite.de.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/aaoConfigLite.de.user.js
+
+## [LSS] Alliance Distance
+
+> Shows difference of total earned credits to the next alliance
+
+| Version  |  Alias / Old names  |                       Download                       |
+|:--------:|:-------------------:|:----------------------------------------------------:|
+| 2022.0.0 | `Alliance-Distance` | [allianceDistance.user.js][allianceDistance.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] Alliance Distance</summary>
+    Zeigt die fehlenden verdienten Credits zum nächsten Verband an
+</details>
+
+[allianceDistance.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/allianceDistance.user.js
+
+## [LSS] Alliance Member Distance
+
+> Shows difference of total earned credits to the next alliance member
+
+| Version  |     Alias / Old names      |                             Download                             |
+|:--------:|:--------------------------:|:----------------------------------------------------------------:|
+| 2022.0.0 | `Alliance-Member-Distance` | [allianceMemberDistance.user.js][allianceMemberDistance.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] Alliance Member Distance</summary>
+    Zeigt die fehlenden verdienten Credits zum nächsten Verbandsmitglied an
+</details>
+
+[allianceMemberDistance.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/allianceMemberDistance.user.js
+
+## [LSS] Alliance Member Notes
+
+> Customizable Notes for each alliance member
+
+| Version  |    Alias / Old names    |                          Download                          |
+|:--------:|:-----------------------:|:----------------------------------------------------------:|
+| 2022.0.0 | `Alliance-Member-Notes` | [allianceMemberNotes.user.js][allianceMemberNotes.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] Alliance Member Notes</summary>
+    Frei anpassbare Notizen für jedes Verbandsmitglied
+</details>
+
+[allianceMemberNotes.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/allianceMemberNotes.user.js
+
+## [LSS] ARR Counter
+
+> Counts how often an ARR is clicked
+
+| Version  |    Alias / Old names     |                 Download                 |
+|:--------:|:------------------------:|:----------------------------------------:|
+| 2022.0.0 | `arr-counter-standalone` | [arrCounter.user.js][arrCounter.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] AAO-Zähler</summary>
+    Zeigt einen Zähler an, wie oft eine AAO geklickt wurde
+</details>
+
+[arrCounter.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/arrCounter.user.js
+
+## [LSS] BSR AAO über Fahrzeugliste
+
+> Macht die AAO über die Fahrzeugliste in Bereitstellungsräumen
+
+| Version  |   Alias / Old names   |                             Download                             |
+|:--------:|:---------------------:|:----------------------------------------------------------------:|
+| 2022.0.0 | `bsr-aao-vehiclelist` | [bsrAAOAboveVehiclelist.user.js][bsrAAOAboveVehiclelist.user.js] |
+
+
+
+[bsrAAOAboveVehiclelist.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/bsrAAOAboveVehiclelist.user.js
+
+## [LSS] Leitstellenansicht Nav-toggle
+
+> Adds a button to show/hide navbar in Leitstellenansicht
+
+| Version  |        Alias / Old names        |                                  Download                                  |
+|:--------:|:-------------------------------:|:--------------------------------------------------------------------------:|
+| 2022.0.0 | `Leitstellenansicht-Nav-toggle` | [navToggleLeitstellenansicht.user.js][navToggleLeitstellenansicht.user.js] |
+
+
+
+[navToggleLeitstellenansicht.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/navToggleLeitstellenansicht.user.js
+
+## [LSS] Reload list of buildings
+
+> Adds a button to the map to reload the list of buildings
+
+| Version  |   Alias / Old names   |                         Download                         |
+|:--------:|:---------------------:|:--------------------------------------------------------:|
+| 2022.0.0 | `reload-buildinglist` | [reloadBuildinglist.user.js][reloadBuildinglist.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] Gebäudeliste neu laden</summary>
+    Fügt der Karte einen Knopf hinzu, über den sich die Gebäudeliste neu laden lässt.
+</details>
+
+[reloadBuildinglist.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/reloadBuildinglist.user.js
+
+## [LSS] Scroll-To-Top buttons
+
+> Shows a scroll-to-top button on all scrollable elements
+
+| Version  | Alias / Old names |                  Download                  |
+|:--------:|:-----------------:|:------------------------------------------:|
+| 2022.0.0 |  `scroll-to-top`  | [scrollToTop.user.js][scrollToTop.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] Scroll-To-Top buttons</summary>
+    Zeigt einen Knopf, um in Elementen nach oben zu scrollen
+</details>
+
+[scrollToTop.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/scrollToTop.user.js
+
+## [LSS] Toplist Distance
+
+> Shows difference of total earned credits to the next user
+
+| Version  | Alias / Old names  |                      Download                      |
+|:--------:|:------------------:|:--------------------------------------------------:|
+| 2022.0.0 | `Toplist-Distance` | [toplistDistance.user.js][toplistDistance.user.js] |
+
+<details>
+    <summary>🇩🇪 [LSS] Toplist Distance</summary>
+    Zeigt die fehlenden verdienten Credits zum nächsten Spieler an
+</details>
+
+[toplistDistance.user.js]: https://github.com/jxn-30/LSS-Scripts/raw/master/src/toplistDistance.user.js
--- util/updateRepo.js
+++ util/updateRepo.js
@@ -26,8 +26,28 @@ const { games } = require('./games');
  * @property {string} path
  */
 
+/**
+ * @typedef ScriptLocale
+ * @property {string} flag
+ * @property {string} name
+ * @property {string} description
+ */
+
+/**
+ * @typedef {ScriptLocale} Script
+ * @property {string} filename
+ * @property {string} url
+ * @property {string} version
+ * @property {string[]} alias
+ * @property {string[]} flagsAvailable
+ * @property {Object.<string, ScriptLocale>} locales
+ */
+
 const ROOT_PATH = path.resolve(__dirname, '..');
 const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+
+/** @type {Script[]} */
+const scriptOverview = [];
 
 const updatedFiles = [];
 
@@ -81,14 +101,15 @@ comments.forEach(comment => {
 
     // add hardlinks
     // unfortunately, hardlinks are required because GitHub doesn't support symbolic links
-    getTags('old').forEach(({ content }) => {
+    const oldNames = getTags('old');
+    oldNames.forEach(({ content }) => {
         const linkPath = path.resolve(ROOT_PATH, `${content}.user.js`);
         if (fs.existsSync(linkPath)) fs.rmSync(linkPath);
         fs.linkSync(`./${path.relative(ROOT_PATH, filePath)}`, linkPath);
     });
 
     // get paths to execute on
-    const locales = getTags('locale');
+    const localesAvailable = getTags('locale');
     const pathMatches = getTags('match', '/*').map(({ tag, content }) => ({
         tag,
         content: content.replace(/\*\\\//g, '*/'),
@@ -97,8 +118,8 @@ comments.forEach(comment => {
     const matches = Object.keys(games)
         .filter(
             game =>
-                locales.length === 0 ||
-                locales.some(({ content }) => content === game)
+                localesAvailable.length === 0 ||
+                localesAvailable.some(({ content }) => content === game)
         )
         .flatMap(game => {
             const { shortURL, police } = games[game];
@@ -122,33 +143,93 @@ comments.forEach(comment => {
             return matches;
         });
 
+    const scriptName = `[LSS] ${comment.name.trim()}`;
+    const localeScriptNames = tags
+        .filter(tag => tag.title.startsWith('name:'))
+        .map(({ title, value }) => ({
+            tag: title,
+            content: `[${
+                Object.entries(games).find(([lang]) =>
+                    lang.startsWith(title.split(':')[1])
+                )?.[1].abbr ?? 'LSS'
+            }] ${value}`,
+        }));
+    const localeDescriptions = getTags('description');
+
+    /** @type {Object.<string, ScriptLocale>} */
+    const localeTranslations = {};
+
+    localeScriptNames.forEach(({ tag, content }) => {
+        const locale = tag.split(':')[1];
+        const game = Object.entries(games).find(([lang]) =>
+            lang.startsWith(locale)
+        );
+        if (!game) return;
+
+        localeTranslations[locale] = {
+            flag: game[1].flag,
+            name: content,
+            description: comment.description,
+        };
+    });
+    localeDescriptions.forEach(({ tag, content }) => {
+        const locale = tag.split(':')[1];
+        const game = Object.entries(games).find(([lang]) =>
+            lang.startsWith(locale)
+        );
+        if (!game) return;
+
+        if (localeTranslations[locale]) {
+            localeTranslations[locale].description = content;
+        } else {
+            localeTranslations[locale] = {
+                flag: game[1].flag,
+                name: scriptName,
+                description: content,
+            };
+        }
+    });
+
+    const versionTag = getTags('version', `${new Date().getFullYear()}.0.0`)[0];
+
+    scriptOverview.push({
+        filename: fileName,
+        name: scriptName,
+        description: comment.description,
+        version: versionTag.content,
+        alias: oldNames.map(({ content }) => content),
+        url: updateURL,
+        flagsAvailable:
+            localesAvailable.length === 0
+                ? []
+                : Object.keys(games)
+                      .filter(game =>
+                          localesAvailable.some(
+                              ({ content }) => content === game
+                          )
+                      )
+                      .map(game => games[game].flag),
+        locales: localeTranslations,
+    });
+
     // list of tags to add to the userscript
     const userscriptHeaderInformation = [
         {
             tag: 'name',
-            content: `[LSS] ${comment.name.trim()}`,
+            content: scriptName,
         },
-        ...tags
-            .filter(tag => tag.title.startsWith('name:'))
-            .map(({ title, value }) => ({
-                tag: title,
-                content: `[${
-                    Object.entries(games).find(([lang]) =>
-                        lang.startsWith(title.split(':')[1])
-                    )?.[1].abbr ?? 'LSS'
-                }] ${value}`,
-            })),
+        ...localeScriptNames,
         {
             tag: 'namespace',
             content: 'https://jxn.lss-manager.de',
         },
-        ...getTags('version', `${new Date().getFullYear()}.0.0`),
+        versionTag,
         ...getTags('author', 'Jan (jxn_30)'),
         {
             tag: 'description',
             content: comment.description,
         },
-        ...getTags('description'),
+        ...localeDescriptions,
         {
             tag: 'homepage',
             content: 'https://github.com/jxn-30/LSS-Scripts',
@@ -196,3 +277,71 @@ ${userscriptTags}
 
     updatedFiles.push(fileName);
 });
+
+const centerString = (string, length) => {
+    const half = Math.floor((length - string.length) / 2);
+    return string.padStart(half + string.length, ' ').padEnd(length, ' ');
+};
+
+fs.writeFileSync(
+    path.resolve(ROOT_PATH, 'README.md'),
+    scriptOverview
+        .sort((a, b) =>
+            a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+        )
+        .map(script => {
+            /** @type {string[][]} */
+            const rows = [
+                ['Version', 'Alias / Old names', 'Download'],
+                [
+                    script.version,
+                    script.alias.map(alias => `\`${alias}\``).join(', '),
+                    `[${script.filename}][${script.filename}]`,
+                ],
+            ];
+            if (script.flagsAvailable.length) {
+                rows[0].splice(1, 0, 'Available in');
+                rows[1].splice(
+                    1,
+                    0,
+                    script.flagsAvailable.map(flag => `\`${flag}\``).join(', ')
+                );
+            }
+            const cellWidths = rows[0].map((_, i) =>
+                Math.max(...rows.map(row => row[i].length))
+            );
+            rows.splice(
+                1,
+                0,
+                cellWidths.map(width => `:${'-'.repeat(width)}:`)
+            );
+            return `
+## ${script.name}
+
+> ${script.description}
+
+${rows
+    .map(
+        row =>
+            `|${row
+                .map((cell, i) => centerString(cell, cellWidths[i] + 2))
+                .join('|')}|`
+    )
+    .join('\n')}
+
+${Object.values(script.locales)
+    .map(({ flag, name, description }) =>
+        `
+<details>
+    <summary>${flag} ${name}</summary>
+    ${description}
+</details>
+`.trim()
+    )
+    .join('\n')}
+
+[${script.filename}]: ${script.url}
+`.trim();
+        })
+        .join('\n\n')
+);
