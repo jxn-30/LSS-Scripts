51c603840b963564d325a147ae2ee3488d3cd2be
jan <jan.boehmerle@ledbrain.de>
2023-05-13 00:37:21 +0200

    feat(scripts): add hideApplicationBackground.user.js

---
+++ src/hideApplicationBackground.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name            [LSS] Hide Application Background
+// @name:de         [LSS] Kein Hintergrund bei Bewerbung
+// @namespace       https://jxn.lss-manager.de
+// @version         2023.05.13+0037
+// @author          Jan (jxn_30)
+// @description     Removes the green background on alliance applications
+// @description:de  Deaktiviert den grünen Hintergrund, wenn der Verband Bewerbungen hat
+// @homepage        https://github.com/jxn-30/LSS-Scripts
+// @homepageURL     https://github.com/jxn-30/LSS-Scripts
+// @icon            https://www.leitstellenspiel.de/favicon.ico
+// @updateURL       https://github.com/jxn-30/LSS-Scripts/raw/master/src/hideApplicationBackground.user.js
+// @downloadURL     https://github.com/jxn-30/LSS-Scripts/raw/master/src/hideApplicationBackground.user.js
+// @supportURL      https://github.com/jxn-30/LSS-Scripts
+// @match           https://www.operacni-stredisko.cz/*
+// @match           https://policie.operacni-stredisko.cz/*
+// @match           https://www.alarmcentral-spil.dk/*
+// @match           https://politi.alarmcentral-spil.dk/*
+// @match           https://www.leitstellenspiel.de/*
+// @match           https://polizei.leitstellenspiel.de/*
+// @match           https://www.missionchief-australia.com/*
+// @match           https://police.missionchief-australia.com/*
+// @match           https://www.missionchief.co.uk/*
+// @match           https://police.missionchief.co.uk/*
+// @match           https://www.missionchief.com/*
+// @match           https://police.missionchief.com/*
+// @match           https://www.centro-de-mando.es/*
+// @match           https://www.centro-de-mando.mx/*
+// @match           https://www.hatakeskuspeli.com/*
+// @match           https://poliisi.hatakeskuspeli.com/*
+// @match           https://www.operateur112.fr/*
+// @match           https://police.operateur112.fr/*
+// @match           https://www.operatore112.it/*
+// @match           https://polizia.operatore112.it/*
+// @match           https://www.missionchief-japan.com/*
+// @match           https://www.missionchief-korea.com/*
+// @match           https://www.nodsentralspillet.com/*
+// @match           https://politiet.nodsentralspillet.com/*
+// @match           https://www.meldkamerspel.com/*
+// @match           https://politie.meldkamerspel.com/*
+// @match           https://www.operatorratunkowy.pl/*
+// @match           https://policja.operatorratunkowy.pl/*
+// @match           https://www.operador193.com/*
+// @match           https://www.jogo-operador112.com/*
+// @match           https://policia.jogo-operador112.com/*
+// @match           https://www.jocdispecerat112.com/*
+// @match           https://www.dispetcher112.ru/*
+// @match           https://www.dispecerske-centrum.com/*
+// @match           https://www.larmcentralen-spelet.se/*
+// @match           https://polis.larmcentralen-spelet.se/*
+// @match           https://www.112-merkez.com/*
+// @match           https://www.dyspetcher101-game.com/*
+// @run-at          document-idle
+// @grant           GM_addStyle
+// ==/UserScript==
+
+/**
+ * @name Hide Application Background
+ * @name:de Kein Hintergrund bei Bewerbung
+ * @description Removes the green background on alliance applications
+ * @description:de Deaktiviert den grünen Hintergrund, wenn der Verband Bewerbungen hat
+ * @old lss-hide-application-background
+ * @grant GM_addStyle
+ */
+
+GM_addStyle(`
+.alliance_apply_new:not(.alliance_forum_new){
+    background: unset !important;
+}
+`);
