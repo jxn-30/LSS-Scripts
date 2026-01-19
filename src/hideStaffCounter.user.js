8772d4953ec365ce12341fc2ceba12dc9ca5a43f
jan <jan.boehmerle@ledbrain.de>
2026-01-19 15:22:38 +0100

    feat: add new script hideStaffCounter

---
+++ src/hideStaffCounter.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name            [LSS] Hide Staff Counter
+// @name:de         [LSS] Personalzähler ausblenden
+// @namespace       https://jxn.lss-manager.de
+// @version         2024.02.14+1250
+// @author          Jan (jxn_30)
+// @description     Hides the staff counter in missions.
+// @description:de  Blendet den Personalzähler in Einsätzen aus.
+// @homepage        https://github.com/jxn-30/LSS-Scripts
+// @homepageURL     https://github.com/jxn-30/LSS-Scripts
+// @icon            https://www.leitstellenspiel.de/favicon.ico
+// @updateURL       https://github.com/jxn-30/LSS-Scripts/raw/master/src/hideStaffCounter.user.js
+// @downloadURL     https://github.com/jxn-30/LSS-Scripts/raw/master/src/hideStaffCounter.user.js
+// @supportURL      https://github.com/jxn-30/LSS-Scripts
+// @match           https://www.operacni-stredisko.cz/missions/*
+// @match           https://policie.operacni-stredisko.cz/missions/*
+// @match           https://www.alarmcentral-spil.dk/missions/*
+// @match           https://politi.alarmcentral-spil.dk/missions/*
+// @match           https://www.leitstellenspiel.de/missions/*
+// @match           https://polizei.leitstellenspiel.de/missions/*
+// @match           https://www.missionchief-australia.com/missions/*
+// @match           https://police.missionchief-australia.com/missions/*
+// @match           https://www.missionchief.co.uk/missions/*
+// @match           https://police.missionchief.co.uk/missions/*
+// @match           https://www.missionchief.com/missions/*
+// @match           https://police.missionchief.com/missions/*
+// @match           https://www.centro-de-mando.es/missions/*
+// @match           https://www.centro-de-mando.mx/missions/*
+// @match           https://www.hatakeskuspeli.com/missions/*
+// @match           https://poliisi.hatakeskuspeli.com/missions/*
+// @match           https://www.operateur112.fr/missions/*
+// @match           https://police.operateur112.fr/missions/*
+// @match           https://www.operatore112.it/missions/*
+// @match           https://polizia.operatore112.it/missions/*
+// @match           https://www.missionchief-japan.com/missions/*
+// @match           https://www.missionchief-korea.com/missions/*
+// @match           https://www.nodsentralspillet.com/missions/*
+// @match           https://politiet.nodsentralspillet.com/missions/*
+// @match           https://www.meldkamerspel.com/missions/*
+// @match           https://politie.meldkamerspel.com/missions/*
+// @match           https://www.operatorratunkowy.pl/missions/*
+// @match           https://policja.operatorratunkowy.pl/missions/*
+// @match           https://www.operador193.com/missions/*
+// @match           https://www.jogo-operador112.com/missions/*
+// @match           https://policia.jogo-operador112.com/missions/*
+// @match           https://www.jocdispecerat112.com/missions/*
+// @match           https://www.dispetcher112.ru/missions/*
+// @match           https://www.dispecerske-centrum.com/missions/*
+// @match           https://www.larmcentralen-spelet.se/missions/*
+// @match           https://polis.larmcentralen-spelet.se/missions/*
+// @match           https://www.112-merkez.com/missions/*
+// @match           https://www.dyspetcher101-game.com/missions/*
+// @run-at          document-idle
+// @grant           GM_addStyle
+// ==/UserScript==
+
+/**
+ * @name Hide Staff Counter
+ * @name:de Personalzähler ausblenden
+ * @description Hides the staff counter in missions.
+ * @description:de Blendet den Personalzähler in Einsätzen aus.
+ * @match /missions/*
+ * @grant GM_addStyle
+ */
+
+GM_addStyle(
+    `small:has(> #amount_of_people) {display: none; & + div {width: 100%}}`
+);
