ad93741e7e996369433b19fc2b792b9cce480906
jan <jan.boehmerle@ledbrain.de>
2026-06-01 15:47:38 +0200

    chore: make vehicle snippet prettier

--- snippets/vehicleValue.js
+++ snippets/vehicleValue.js
@@ -6,11 +6,8 @@ vt = await fetch('https://api.lss-manager.de/de_DE/vehicles').then(res =>
     res.json()
 );
 console.log(
-    v.length +
-        ' Fahrzeuge mit einem Gesamtwert von ' +
-        v
-            .map(v => vt[v.vehicle_type].credits)
-            .reduce((a, b) => a + b, 0)
-            .toLocaleString() +
-        ' Credits.'
+    `${v.length.toLocaleString()} Fahrzeuge mit einem Gesamtwert von ${v
+        .map(v => vt[v.vehicle_type].credits)
+        .reduce((a, b) => a + b, 0)
+        .toLocaleString()} Credits.`
 );
